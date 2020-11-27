9b752b18e293c2828731231ccf8096af703fd09b
pontuscroneld <pontuscroneld@gmail.com>
2020-11-27 13:08:10 +0100

    Initial Commit

---
+++ GruppProjektSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GruppProjektSwiftUI
+//
+//  Created by Pontus Croneld on 2020-11-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GruppProjektSwiftUI/GruppProjektSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GruppProjektSwiftUIApp.swift
+//  GruppProjektSwiftUI
+//
+//  Created by Pontus Croneld on 2020-11-27.
+//
+
+import SwiftUI
+
+@main
+struct GruppProjektSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
