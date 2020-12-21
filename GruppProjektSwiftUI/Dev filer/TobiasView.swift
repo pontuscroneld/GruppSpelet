f36c0223064d94d6bf7ff7aa5501f2b7dd70208c
Tobias Lehning <tobias.lehning@gmail.com>
2020-12-21 12:49:41 +0100

    ändrat bakgrund och lite grafik

--- GruppProjektSwiftUI/Dev filer/TobiasView.swift
+++ GruppProjektSwiftUI/Dev filer/TobiasView.swift
@@ -11,62 +11,119 @@ struct TobiasView: View {
     var body: some View {
         
         ZStack {
-            Image("bg2")
+            Image("background")
                 .resizable()
                 .ignoresSafeArea()
-                .scaledToFill()
+            
+            
             
             VStack {
-                
-                //TODO: Image("Logga")
-                
                 Spacer()
-                
-//                Text("VÄLJ KATEGORI")
-//                    .font(.largeTitle)
-//                    .fontWeight(.bold)
-//                    .foregroundColor(.white)
-//                    .padding()
-//                    .background(Color("AccentColor"))
-//
-//
-//
                 Spacer()
+                ScrollView{
+                    Button(action: {
+                        print("Tryckt på Former")
+                    }) {
+                        Text(Image("former"))
+                            .fontWeight(.bold)
+                            .font(.system(size: 60))
+                            .frame(width: 300, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            //.cornerRadius(25)
+                            //.foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 3)
+                                    .stroke(Color.white, lineWidth: 3)
+                            )
+                    }
+                   // .border(Color.white, width: 5)
+                    .padding()
+                    .shadow(radius: 10)
+                    .shadow(radius: 10)
+                  
                     
+                    Button(action: {
+                        print("Tryckt på Former")
+                    }) {
+                        Text("FÄRGER")
+                            .fontWeight(.bold)
+                            .font(.system(size: 60))
+                            .frame(width: 300, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .cornerRadius(25)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.red)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 3)
+                                    .stroke(Color.white, lineWidth: 3)
+                            )
+                    }
+                   // .border(Color.white, width: 5)
+                    .padding()
+                    .shadow(radius: 10)
+                    .shadow(radius: 10)
                     
-                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-                    cat_button()
-                }
-                .padding(.bottom, 100)
-                
-                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-                    cat_button()
-                }
-                .padding(.bottom, 100)
-                
-                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-                    cat_button()
-                }
-                .padding(.bottom, 100)
-                
-                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-                    cat_button()
-                        
-                }
+                    Button(action: {
+                        print("Tryckt på Former")
+                    }) {
+                        Text("TEST")
+                            .fontWeight(.bold)
+                            .font(.system(size: 60))
+                            .frame(width: 300, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .cornerRadius(25)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.green)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 3)
+                                    .stroke(Color.white, lineWidth: 3)
+                            )
+                    }
+                   // .border(Color.white, width: 5)
+                    .padding()
+                    .shadow(radius: 10)
+                    .shadow(radius: 10)
+                    
+                    Button(action: {
+                        print("Tryckt på Former")
+                    }) {
+                        Text("TEST")
+                            .fontWeight(.bold)
+                            .font(.system(size: 60))
+                            .frame(width: 300, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .cornerRadius(25)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.yellow)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 3)
+                                    .stroke(Color.white, lineWidth: 3)
+                            )
+                    }
+                   // .border(Color.white, width: 5)
+                    .padding()
+                    .shadow(radius: 10)
+                    .shadow(radius: 10)
+                }.padding(.bottom, 40.0).frame(width: .infinity, height:800, alignment: .center)
+                //.background(Color.gray)
                 Spacer()
-                
-                    
-                
             }
-            
         }
+        
     }
     
 }
 
 
+
+
+
+
+
+
 struct TobiasView_Previews: PreviewProvider {
     static var previews: some View {
-      TobiasView()
+        ContentView()
     }
 }
